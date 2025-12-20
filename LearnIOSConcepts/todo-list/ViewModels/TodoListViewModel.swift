55cc73ec351e3c5f8003270dcfb51d2969cff243
Ahmed Ibrahim <ahmadibrahim2020@yahoo.com>
2025-12-20 10:42:25 +0200

    feat: moved fetch to the VM

--- LearnIOSConcepts/todo-list/ViewModels/TodoListViewModel.swift
+++ LearnIOSConcepts/todo-list/ViewModels/TodoListViewModel.swift
@@ -1,3 +1,35 @@
+import CoreData
 import SwiftUI
 
-class TodoListViewModel: ObservableObject {}
+class TodoListViewModel: NSObject, ObservableObject {
+    @Published var tasks: [TaskItem] = []
+    
+    private let context = PersistenceController.shared.container.viewContext
+    private let frc: NSFetchedResultsController<TaskItem>
+    
+    override init() {
+        let request = TaskItem.fetchRequest()
+        request.sortDescriptors = []
+        
+        self.frc = NSFetchedResultsController(fetchRequest: request, managedObjectContext: context, sectionNameKeyPath: nil, cacheName: nil)
+        
+        super.init()
+        
+        frc.delegate = self
+        
+        do {
+           try frc.performFetch()
+            tasks = frc.fetchedObjects ?? []
+        } catch {
+            print("Failed to fetch tasks \(error)")
+        }
+    }
+}
+
+extension TodoListViewModel: NSFetchedResultsControllerDelegate {
+    func controllerDidChangeContent(_ controller: NSFetchedResultsController<any NSFetchRequestResult>) {
+        if let fetchedTask = controller.fetchedObjects as? [TaskItem] {
+            tasks = fetchedTask
+        }
+    }
+}
--- LearnIOSConcepts/todo-list/Views/TodoList.swift
+++ LearnIOSConcepts/todo-list/Views/TodoList.swift
@@ -2,23 +2,18 @@ import SwiftUI
 
 struct TodoList: View {
     @StateObject var viewModel: TodoListViewModel
-    // Sort by time
-    @FetchRequest(
-        sortDescriptors: [],
-        animation: .default)
-    var tasks: FetchedResults<TaskItem>
     @State private var showAddTask = false
 
     var body: some View {
         NavigationView {
             ZStack {
                 VStack {
-                    if tasks.isEmpty {
+                    if viewModel.tasks.isEmpty {
                         VStack {
                             Text("No tasks added yet")
                         }
                     } else {
-                        List(tasks.sorted()) {
+                        List(viewModel.tasks.sorted()) {
                             TaskListRow(task: $0)
                         }
                     }
